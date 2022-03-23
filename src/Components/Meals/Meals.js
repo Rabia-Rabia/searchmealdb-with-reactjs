84e3e9e7806bd29abb163a9ac389b62125fcb1ec
Rabia-Rabia <76133508+Rabia-Rabia@users.noreply.github.com>
2022-03-23 14:58:43 +0400

    Update Meals.js

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -3,13 +3,13 @@ import "./Meal.css"
 
 class Meals extends Component {
     render() {
-        const {meal, OrderNow} = this.props
+        const {meal} = this.props
         return (
             <div className="meals">
             <img src={meal.strMealThumb} alt="" />
             <h4>{meal.strMeal}</h4>
             <p>{meal.strArea}</p>
-            <button onClick={() => OrderNow(meal)}>Add this Food</button>
+            <button>Add this Food</button>
         </div>
         );
     }
